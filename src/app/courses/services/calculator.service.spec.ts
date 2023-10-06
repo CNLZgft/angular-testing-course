3831b9d12a83b896cf813217c5eb2a3c06817c63
CNLZgft <cristian.lopez@gft.com>
2023-10-06 15:47:15 +0200

    se modifica la integración de las depencias de los servicios "CalculatorService" y "LoggerService"  basándose en la inyección de depencias de ANgular

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,21 +1,45 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
+import { TestBed } from "@angular/core/testing";
+
+let calculator: CalculatorService;
+let loggerSpy: any;
 
 describe("CalculatorService", () => {
-  it("sould add two numbers", () => {
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
+  beforeEach(() => {
+    console.log("calling beforeEach");
 
-    const calculator = new CalculatorService(logger);
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+
+    //"TestBed" allow us to provide the dependencies to our services by using dependency
+    //injection instead of calling constructors exlicitly
+
+    //al final se basa en la dependency injection de Angular, en lugar de usar la
+    //implementación de depencias por defecto de Jasmine
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        //de esta forma se puede usar el servicio inyectándolo en la propiedad
+        //"useValue"
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+
+    calculator = TestBed.inject(CalculatorService);
+  });
+
+  it("sould add two numbers", () => {
+    console.log("add test");
 
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("sould substract two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    console.log("substract test");
 
     const result = calculator.subtract(2, 2);
 
