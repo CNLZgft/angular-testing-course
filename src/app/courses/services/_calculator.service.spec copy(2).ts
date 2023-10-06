f207f68d75c237bf97d7b486392b6acbfa56d0db
CNLZgft <cristian.lopez@gft.com>
2023-10-06 16:00:35 +0200

    copia adicional del fichero "spec" para dejar las anotaciones. commonly used test utilities

--- src/app/courses/services/_calculator.service.spec copy(2).ts
+++ src/app/courses/services/_calculator.service.spec copy(2).ts
@@ -5,16 +5,32 @@ import { TestBed } from "@angular/core/testing";
 let calculator: CalculatorService;
 let loggerSpy: any;
 
+//xdescribe --> para deshabilitar todo el conjunto de tests
+//xit --> para deshabilitar un test específico
+//fdescribe --> para ejecutar un sólo conjunto de tests, en el caso de que hubiera más de 1
+//fit --> para ejectuar un test específico
+
 describe("CalculatorService", () => {
   beforeEach(() => {
     console.log("calling beforeEach");
 
+    //se hace un mock (fake dependency) del servicio LoggerService porque realmente se quiere
+    //hacer unit testing del servicio CalculatorService
+    //si se quisiera usar el servicio real de LoggerService entonces esto pasaría a ser
+    //un Integration Test, ya que intervendrían los dos servicios así como la
+    //comunicación entre ellos
     loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
 
+    //"TestBed" allow us to provide the dependencies to our services by using dependency
+    //injection instead of calling constructors exlicitly
+
+    //al final se basa en la dependency injection de Angular, en lugar de usar la
+    //implementación de depencias por defecto de Jasmine
     TestBed.configureTestingModule({
       providers: [
         CalculatorService,
-
+        //de esta forma se puede usar el servicio inyectándolo en la propiedad
+        //"useValue"
         { provide: LoggerService, useValue: loggerSpy },
       ],
     });
